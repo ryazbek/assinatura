3c348a4604a212c9c1b953064a3521806ff45b5f
Helder Rodrigues <helder.rodrigues@ryazbek.com.br>
2025-02-26 11:41:39 -0300

    me-qr api remove

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const form = document.getElementById("signatureForm");
     const previewContainer = document.getElementById("signature");
-    const qrContainer = document.getElementById("qrcode");
 
     function updatePreview() {
         const nome = document.getElementById("nome").value || "Seu Nome";
@@ -20,53 +19,6 @@ document.addEventListener("DOMContentLoaded", function () {
             <span style="color:#696969;">${endereco}</span>
         `;
     }
-
-    /*async function gerarQRCodeMeQR(nome, cargo, email, telefone, endereco) {
-        const meQrToken = "102ea313e6bc9b5ec8673cf8aef71762f9f398b5e0cc103809f0f5ec42bfb2cc";
-        const qrData = {
-            token: meQrToken,
-            qrType: 2,
-            title: "Assinatura de E-mail",
-            service: "api",
-            format: "png",
-            qrOptions: {
-                color: "#696969",
-                logo: "https://ryazbek.github.io/logo_y.png"
-            },
-            qrFieldsData: {
-                vcard: {
-                    first_name: nome,
-                    last_name: "",
-                    job_title: cargo,
-                    email: email,
-                    phone: telefone,
-                    address: endereco
-                }
-            }
-        };
-
-        try {
-            const response = await fetch("https://me-qr.com/api/v1/create", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(qrData)
-            });
-
-            const result = await response.json();
-
-            if (result && result.qr_code_url) {
-                return result.qr_code_url;
-            } else {
-                console.error("Erro ao gerar o QR Code:", result);
-                return "";
-            }
-        } catch (error) {
-            console.error("Erro na requisição ao Me-QR:", error);
-            return "";
-        }
-    }*/
 
     document.querySelectorAll("input").forEach(input => {
         input.addEventListener("input", updatePreview);
@@ -86,31 +38,12 @@ document.addEventListener("DOMContentLoaded", function () {
             return;
         }
 
-        /* Swal.fire({
-            title: "Aguarde...",
-            text: "Gerando QR Code e enviando email.",
-            allowOutsideClick: false,
-            didOpen: () => {
-                Swal.showLoading();
-            }
-        });
-
-        const qrCodeUrl = await gerarQRCodeMeQR(nome, cargo, email, telefone, endereco);
-
-        if (!qrCodeUrl) {
-            Swal.fire("Erro!", "Falha ao gerar o QR Code.", "error");
-            return;
-        } 
-
-        qrContainer.innerHTML = `<img src="${qrCodeUrl}" alt="QR Code">`; */
-
         const templateParams = {
             nome_html: nome,
             cargo_html: cargo,
             user_html: email,
             tel_html: telefone,
-            address_html: endereco,
-            // qr_html: `<img src="${qrCodeUrl}" alt="QR Code">`
+            address_html: endereco
         };
 
         emailjs.send("service_eegaehm", "template_cck7sxv", templateParams)
