d5e2a61bf24180b92fb7fb6c280a9d095ef352a6
Helder Rodrigues <helder.rodrigues@ryazbek.com.br>
2025-02-28 15:10:05 -0300

    finish

--- obrigado.js
+++ obrigado.js
@@ -1,13 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Obtém o e-mail do usuário passado pela URL
-    const urlParams = new URLSearchParams(window.location.search);
-    const userEmail = urlParams.get("email") || "seu.email@ryazbek.com.br";
-
-    // Insere o e-mail na mensagem
-    document.getElementById("userEmail").textContent = userEmail;
-    document.getElementById("userEmailCopy").textContent = userEmail;
-
-    // Configura o botão para exibir/esconder o passo a passo
     const howToContainer = document.getElementById("howToContainer");
     const toggleButton = document.getElementById("toggleHowTo");
 
